b6867152e4b1e68ca29f49bcb34da33246b61e86
rajashreeupm <rajashreeupm@gmail.com>
2022-03-08 21:07:27 -0500

    JS files Repo

---
+++ JS_Obj_Rajashree.js
@@ -0,0 +1,18 @@
+var house ={
+    type :['condo','individual','ranch','Apartment'],
+    rent : 1200,
+    isCarParkingAvailable : true,
+    parkingType : ['street','Covered'],
+    Bed : 3,
+    Bath : 2.5,
+    Address : {
+    Address1 : "123 mill drive",
+    Address2 : 'C10',
+    city: 'Hartford',
+    state: 'CT',
+    zipCode: '06103',
+    }
+}
+console.log(house)
+console.log(house.parkingType[1])
+console.log(house.Address.zipCode)
---
+++ intro.js
@@ -0,0 +1,11 @@
+var a=10
+var b=20
+console.log(a+b)
+for(i=0;i<10;i++)
+{
+    if(i!=0)
+    {
+
+console.log("The number is " +i)
+}
+}
